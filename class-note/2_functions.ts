6c8b7885098c1b0f2265caaa7a0f296c34473a6b
HyeonJunKOH <hyeonjun9194@gmail.com>
2024-04-25 17:10:31 +0900

    4월25일 타입스크립트2일차

---
+++ class-note/2_functions.ts
@@ -0,0 +1,9 @@
+// 함수의 변환 값에 타입을 정의하는 방식
+function add():number{
+    return 10;
+}
+
+// 함수에 타입을 정의하는 방식
+function sum(a:number,b:number):number{
+    return a + b;
+}
--- why-ts/app.js
+++ why-ts/app.js
@@ -9,13 +9,17 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+
 function startApp() {
   axios
     .get(url)
     .then(function (response) {
-      console.log(response);
+      // console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user);
+      username.innerText = user[0].name;
+      email.innerText = user[0].email;
     })
     .catch(function (error) {
       console.log(error);
